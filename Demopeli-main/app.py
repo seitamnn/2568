7109740d3650d255281300d64995bec188ef458e
Mmiud <mmiude@outlook.com>
2024-05-03 19:30:40 +0300

    kaiken näköstä pikkukivaa

--- Demopeli-main/app.py
+++ Demopeli-main/app.py
@@ -1,6 +1,7 @@
 import json
 import os
 import random
+import uuid
 
 import mysql.connector
 from dotenv import load_dotenv
@@ -28,7 +29,7 @@ config.conn = mysql.connector.connect(
          autocommit=True
          )
 
-def fly(id, dest, consumption=0, player=None):
+def fly(id, dest, consumption=0, player=None): #ei käytössä, voi poistaa jollei keksi käyttöä
     if id==0:
         game = Game(0, dest, consumption, player)
     else:
@@ -40,7 +41,7 @@ def fly(id, dest, consumption=0, player=None):
     json_data = json.dumps(game, default=lambda o: o.__dict__, indent=4)
     return json_data
 
-def select_airport():
+def select_airport(): #valitaan kolme kenttää randomilla
     sql = f"SELECT airport.name, airport.latitude_deg, airport.longitude_deg, country.name FROM airport JOIN country ON airport.iso_country = country.iso_country WHERE country.name != 'Norway'"
     cursor = config.conn.cursor()
     cursor.execute(sql)
@@ -65,43 +66,21 @@ def select_airport():
     ]
     return airport_choices
 
-
-# http://127.0.0.1:5000/flyto?game=fEC7n0loeL95awIxgY7M&dest=EFHK&consumption=123
-@app.route('/flyto')
-def flyto():
-    args = request.args
-    id = args.get("game")
-    dest = args.get("dest")
-    consumption = args.get("consumption")
-    json_data = fly(id, dest, consumption)
-    print("*** Called flyto endpoint ***")
-    return json_data
-
-
-# http://127.0.0.1:5000/newgame?player=Vesa&loc=EFHK
-@app.route('/newgame')
-def newgame():
-    args = request.args
-    player = args.get("player")
-    dest = args.get("loc")
-    json_data = fly(0, dest, 0, player)
-    return json_data
-
-
-@app.route('/player_info/<screen_name>/') #blabla
-def user_currency_distance(screen_name):
-    sql = (f"SELECT screen_name, currency, alien_distance, location, in_possession FROM game WHERE screen_name = '{screen_name}'")
+def player_info(screen_name): #pelaajan tiedot
+    sql = (
+        f"SELECT screen_name, currency, alien_distance, location, in_possession FROM game WHERE screen_name = '{screen_name}'")
     cursor = config.conn.cursor()
     cursor.execute(sql)
     userdata = cursor.fetchall()
-    player_info = {
+    player_data = {
         "screen_name": userdata[0][0],
         "currency": userdata[0][1],
         "alien_distance": userdata[0][2],
         "location": userdata[0][3],
         "in_possession": userdata[0][4]
     }
-    sql2 = (f"SELECT airport.name, airport.latitude_deg, airport.longitude_deg, country.name FROM airport JOIN country ON airport.iso_country = country.iso_country WHERE ident='{userdata[0][3]}'")
+    sql2 = (
+        f"SELECT airport.name, airport.latitude_deg, airport.longitude_deg, country.name FROM airport JOIN country ON airport.iso_country = country.iso_country WHERE ident='{userdata[0][3]}'")
     cursor.execute(sql2)
     current_airport = cursor.fetchall()
     player_location = {
@@ -110,11 +89,53 @@ def user_currency_distance(screen_name):
         "airport_longitude": current_airport[0][2],
         "country_name": current_airport[0][3]
     }
-    status = {"playerInfo": player_info, "airport": select_airport(), "currentLocation": player_location}
+    status = {"playerInfo": player_data, "airport": select_airport(), "currentLocation": player_location}
     return status
 
 
-@app.route('/select_norway/')
+def flyto(screen_name, dest, price): #lennetään
+    sql2 = f"UPDATE game SET location = (SELECT ident FROM airport WHERE name = '{dest}') WHERE screen_name = '{screen_name}';"
+    cursor = config.conn.cursor()
+    cursor.execute(sql2)
+    sql3 = f"UPDATE game SET currency = currency - '{price}' WHERE screen_name = '{screen_name}'"
+    cursor.execute(sql3)
+    return player_info(screen_name)
+
+
+def start(screen_name): #tää käyttöön, kun saadaan pelaajan syöttämä nimi talteen
+    def generate_player_id():
+        return str(uuid.uuid4())
+    mycursor = config.conn.cursor()
+    sql = f"INSERT INTO game (id, location, screen_name, currency, alien_distance, in_possession) VALUES (%s, %s, %s, %s, %s,%s)" #noihin laitetaa arvot jotka annetaa seuraavalla rivillä
+    mycursor.execute(sql, (generate_player_id(), 'MUHA', screen_name, 500, 6, False))  # MUHA = José Martí International Airport
+    mycursor.fetchall()
+    return flyto(screen_name, "José Martí International Airport", 0)
+
+
+# http://127.0.0.1:5000/newgame?player=Vesa&loc=EFHK
+@app.route('/newgame') # tää käyttöön kun saadaan pelaajan täyttämä nimi talteen
+def newgame():
+    args = request.args
+    player = args.get("player")
+    return start(player)
+
+
+# http://127.0.0.1:5000/fly_to?playerName=fEC7n0loeL95awIxgY7M&dest=EFHK&price=10
+@app.route('/fly_to') #käytössä, kun lennetään
+def fly_to():
+    args = request.args
+    screen_name = args.get("playerName")
+    dest = args.get("dest")
+    price = int(args.get("price"))
+    return flyto(screen_name, dest, price)
+
+
+@app.route('/player_info/<screen_name>/') # pelaajan tiedot haetaan tällä
+def player_test(screen_name):
+    return player_info(screen_name)
+
+
+@app.route('/select_norway') # ei ainakaan vielä käytössä
 def select_airport_norway():
     try:
         sql = f"SELECT airport.name, country.name FROM airport JOIN country ON airport.iso_country = country.iso_country WHERE country.name = 'Norway'"
@@ -136,7 +157,7 @@ def select_airport_norway():
     return Response(response=json_response, status=status_code, mimetype='application/json')
 
 
-@app.route('/select_cuba/')
+@app.route('/select_cuba/') # ei vielä käytössä
 def select_airport_cuba():
     try:
         sql = f"SELECT airport.name, country.name FROM airport JOIN country ON airport.iso_country = country.iso_country WHERE country.name = 'Cuba'"
@@ -156,17 +177,6 @@ def select_airport_cuba():
         }
     json_response = json.dumps(cuba_airport)
     return Response(response=json_response, status=status_code, mimetype='application/json')
-
-
-@app.errorhandler(404)
-def not_found(error):
-    response = {
-        "status": error.status_code,
-        "teksti": error.message
-    }
-    json_response = json.dumps(response)
-    return Response(response=json_response, status=error.status_code, mimetype='application/json')
-
 
 
 if __name__ == '__main__':
--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
 }).addTo(map);
 map.setView([60, 24], 7);
 
-
 // global variables
 
 // icons
@@ -16,7 +15,7 @@ const redIcon = L.divIcon({className: 'red-icon'});
 const greenIcon = L.divIcon({className: 'green-icon'});
 
 
-// form for player name
+// form for player name  todo: tässä tarkoktuksena saada kiinni pelaajan syöttämästä nimestä ja tän jälkeen kutsutaan funktiota gameSetup kerran täällä kun nimi syötetty newgame päätepisteellä -> http://127.0.0.1:5000/newgame?player=kakkapylly
 
 // function to fetch data from API
 async function getData(url){
@@ -36,45 +35,70 @@ function updatePlayerInfo(data){
   distance.innerHTML = data.playerInfo.alien_distance;
 }
 
-// function to show weather at selected airport
-
-// function to check if any goals have been reached
-
 // function to update airports in UI
 function updateAirports(airports){
   const airport1 = document.querySelector('label[for=airport1]');
   const airport2 = document.querySelector('label[for=airport2]');
   const airport3 = document.querySelector('label[for=airport3]');
-  console.log(airports.airport[0].airport_name);
-  airport1.innerHTML = `Airport: ${airports.airport[0].airport_name}<br>Country: ${airports.airport[0].country_name}`;
-  airport2.innerHTML = `Airport: ${airports.airport[1].airport_name}<br>Country: ${airports.airport[1].country_name}`;
-  airport3.innerHTML = `Airport: ${airports.airport[2].airport_name}<br>Country: ${airports.airport[2].country_name}`;
+  airport1.innerHTML = `Airport: ${airports.airport[0].airport_name}<br>Country: ${airports.airport[0].country_name}  `;
+  airport2.innerHTML = `Airport: ${airports.airport[1].airport_name}<br>Country: ${airports.airport[1].country_name}  `;
+  airport3.innerHTML = `Airport: ${airports.airport[2].airport_name}<br>Country: ${airports.airport[2].country_name}  `;
+
+  document.querySelector('#airport1').addEventListener('click',function(evt){
+    gameSetup(`http://127.0.0.1:5000/fly_to?playerName=${airports.playerInfo.screen_name}&dest=${airports.airport[0].airport_name}&price=10`); // lähetetään serverille pyyntö fly_to päätepisteeseen mikä ohjaa päivittämään pelaajan sijainnin
+    console.log(airports.airport[0].airport_name);
+    console.log(airports.playerInfo.screen_name);
+  })
+  document.querySelector('#airport2').addEventListener('click',function(evt){ //todo: näiden kanssa vielä ongelmia! hakee jostain syystä aina edellisen kierroksen tiedot, jolloin lentäminen ei onnistu, muutakun ekalle kentälle :'(
+    gameSetup(`http://127.0.0.1:5000/fly_to?playerName=${airports.playerInfo.screen_name}&dest=${airports.airport[1].airport_name}&price=10`);
+    console.log(airports.airport[1].airport_name);
+    console.log(airports.playerInfo.screen_name)
+  })
+  document.querySelector('#airport3').addEventListener('click',function(evt){
+    gameSetup(`http://127.0.0.1:5000/fly_to?playerName=${airports.playerInfo.screen_name}&dest=${airports.airport[2].airport_name}&price=10`)
+    console.log(airports.airport[2].airport_name);
+    console.log(airports.playerInfo.screen_name)
+  })
 }
 // function to check if game is over
+function checkGameOver (currency, distance){ // todo: pitää tarkistaa toimiiko tällä. Epäilen. XD
+  if (currency < 0 || distance < 0){
+    alert(`Game over!`);
+    return false;
+  }
+  return true;
+}
 
 // function to set up game
 // this is the main function that creates the game and calls the other functions
-async function gameSetup() {
+async function gameSetup(url) {
   try {
-    const playerInfo = await getData('http://127.0.0.1:5000/player_info/Pumba/');
+    const playerInfo = await getData(url);
     console.log(playerInfo);
     updatePlayerInfo(playerInfo); // päivitetään pelaaja tiedot näytölle
+    if(!checkGameOver(playerInfo.playerInfo.currency, playerInfo.playerInfo.alien_distance)) return;
 
     const pmarker = L.marker([playerInfo.currentLocation.airport_latitude, playerInfo.currentLocation.airport_longitude]).addTo(map); // pelaajan sijainti kartalle
     pmarker.bindPopup(`<b>You are here!</b></b><br>Airport: ${playerInfo.currentLocation.airport_name}<br>Country: ${playerInfo.currentLocation.country_name}`); // popup ikkuna missä näkyy sijainnin tiedot
-    pmarker.openPopup();
+    pmarker.openPopup(); // you are here ikkuna aukee automaattisesti
     pmarker.setIcon(greenIcon);
     for(let location of playerInfo.airport){
       const marker = L.marker([location.latitude, location.longitude]).addTo(map); // uudet kohteet kartalle
       marker.bindPopup(`Airport: ${location.airport_name}<br>Country: ${location.country_name}`); // popup missä näkyy kohteiden tiedot
       marker.setIcon(redIcon)
     }
-    updateAirports(playerInfo);
+
+    updateAirports(playerInfo); // random lentokentät location boxiin
 
   } catch (error){
     console.log(error)
   }
 }
-gameSetup();
+gameSetup(`http://127.0.0.1:5000/player_info/nipsu`) //kutsutaan pääfunktiota - toimii vaan index.html sivulla ja nipsun tilalle pitää laittaa joku pelaajanimi mikä löytyy omasta tietokannasta.
+
+
 // event listener to hide goal splash
 //blablaa
+
+//http://127.0.0.1:5000/newgame?player=Vesa&loc=EFHK
+//'http://127.0.0.1:5000/player_info/Pumba/'
